9dd007a5a1d500e7f897062d3e919faac5162329
Amr Mostafa <amrmos.it@gmail.com>
2025-06-09 03:10:37 +1000

    Configuring serilog library for logging messages

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog; // Add Serilog for logging // serilog is a logging library that provides a simple way to log messages in .NET applications. 
 
+var builder = WebApplication.CreateBuilder(args);
+// Configure Serilog for logging
+// Serilog is a logging library that provides a simple way to log messages in .NET applications.
+long.Logger = new LoggerConfiguration()
+    .WriteTo.Console() // Write logs to the console
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Write logs to a file with daily rolling
+    .CreateLogger();
 
 
 var app = builder.Build();
