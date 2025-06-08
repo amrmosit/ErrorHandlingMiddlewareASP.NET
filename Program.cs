ba9d9e9b2b01a19c21e00932e3af989b4952fb09
Amr Mostafa <amrmos.it@gmail.com>
2025-06-09 03:14:17 +1000

    Configuring the application pipeline

--- Program.cs
+++ Program.cs
@@ -8,12 +8,17 @@ long.Logger = new LoggerConfiguration()
     .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Write logs to a file with daily rolling
     .CreateLogger();
 
+// pipeline configuration
+builder.Host.UseSerilog(); // Use Serilog for logging
+builder.Services.AddControllers(); // Add controllers to the service collection
+builder.Logging.ClearProviders(); // Clear default logging providers
+builder.Logging.AddConsole(); // Add console logging provider
 
+// build the application
+// The Build method creates the application instance based on the configuration and services defined in the builder.
 var app = builder.Build();
-
-
-
 app.UseHttpsRedirection();
+
 
 
 app.Run();
